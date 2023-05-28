cd8567548fb04c1d2fadac149be132b7ac778f40
PoloLavapies <iberia9lavapies@gmail.com>
2023-05-28 11:58:48 +0900

    最初の画面はフラグメントを使って表示できるようになった

--- app/src/main/java/com/example/kakeibo/activity/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo/activity/MainActivity.kt
@@ -1,196 +1,34 @@
 package com.example.kakeibo.activity
 
-import android.content.Intent
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.text.SpannedString
-import android.widget.Button
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.text.buildSpannedString
-import androidx.core.text.color
+import androidx.fragment.app.FragmentTransaction
 import com.example.kakeibo.R
-import com.example.kakeibo.database.KakeiboDatabase
-import com.example.kakeibo.entity.Category
-import com.kal.rackmonthpicker.RackMonthPicker
-import com.kal.rackmonthpicker.listener.DateMonthDialogListener
-import com.kal.rackmonthpicker.listener.OnCancelMonthDialogListener
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Locale
+import com.example.kakeibo.fragment.MainFragment
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val spendingCategoryIds = mutableListOf<Int>()
-    private val incomeCategoryIds = mutableListOf<Int>()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addButton = findViewById<Button>(R.id.add_button)
-        addButton.setOnClickListener {
-            val intent = Intent(application, AddActivity::class.java)
-            startActivity(intent)
-        }
-
         val toolBar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolBar)
-    }
 
-    override fun onResume() {
-        super.onResume()
+        val fragment = MainFragment.newInstance(0, 0)
+        val args = Bundle()
+        args.putInt("year", 0)
+        args.putInt("month", 0)
+        fragment.arguments = args
 
-        var today: LocalDate = LocalDate.now()
-        val year: Int = intent.getIntExtra("year", 0)
-        val month: Int = intent.getIntExtra("month", 0)
-
-        // TODO 変数の重複があるので、変えるかコメントを書いた方が良い
-        val date = if (year != 0 && month != 0) {
-            LocalDate.of(year, month, 1)
-        } else {
-            today
-        }
-
-        // 年と月の表示
-        setMonthView(date)
-
-        // 表の生成 (日と金額の表示)
-        initCategoryList()
-        val dayList: List<Int> = getDayList(date)
-
-        // 当月の日かどうかのフラグ (先月末から来月頭の日をループで処理するにあたり必要)
-        var thisMonthFlag: Boolean = false
-        for (i in 0 until dayList.size / 7) {
-            for (j in 0..6) {
-                val dayOfMonth = dayList.get(i * 7 + j)
-                if (dayOfMonth == 1) {
-                    thisMonthFlag = !thisMonthFlag
-                }
-
-                val textViewId = resources.getIdentifier("date${i}_${j}", "id", packageName)
-                val dateView: TextView = findViewById(textViewId)
-                dateView.text = dayList.get(i * 7 + j).toString()
-
-                if (thisMonthFlag) {
-                    val buttonId = resources.getIdentifier("button${i}_${j}", "id", packageName)
-                    val button: Button = findViewById(buttonId)
-                    val date: LocalDate = date.withDayOfMonth(dayOfMonth)
-                    button.text = getSpentMoneyText(date)
-                    button.setOnClickListener {
-                        val intent = Intent(application, DetailActivity::class.java)
-                        intent.putExtra("date", date.format(DateTimeFormatter.ISO_DATE))
-                        startActivity(intent)
-                    }
-                } else {
-                    dateView.setTextColor(Color.parseColor("lightgray"))
-                }
-            }
-        }
-    }
-
-    // TODO このコメントは必要なのか?
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun setMonthView(date: LocalDate) {
-        val monthView: TextView = findViewById(R.id.month);
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("YYYY年MM月")
-        monthView.text = date.format(formatter)
-
-        // クリック時の処理
-        monthView.setOnClickListener() {
-            showMonthPickerDialog(date)
-        }
-    }
-
-    private fun showMonthPickerDialog(date: LocalDate) {
-        RackMonthPicker(this)
-            // JAPANESEかも
-            .setLocale(Locale.ENGLISH)
-            // TODO Yearは機能する Monthは機能していない
-            .setSelectedMonth(date.monthValue)
-            .setSelectedYear(date.year)
-            .setPositiveButton(DateMonthDialogListener
-            { month, startDate, endDate, year, monthLabel ->
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("year", year)
-                intent.putExtra("month", month)
-                startActivity(intent)
-            })
-            .setNegativeButton(OnCancelMonthDialogListener
-            { dialog ->
-                dialog.dismiss()
-            })
-            .show()
-    }
-
-    // 前後の月を含む、6週分の日のリストを返す (日曜始まり)
-    private fun getDayList(dayOfThisMonth: LocalDate): List<Int> {
-        val firstDayOfMonth: LocalDate = LocalDate.of(dayOfThisMonth.year, dayOfThisMonth.month, 1)
-        val lastDayOfMonth: LocalDate = firstDayOfMonth.plusMonths(1).minusDays(1)
-
-        var dayList: List<Int> = arrayListOf()
-
-        var date: LocalDate = firstDayOfMonth.minusDays((firstDayOfMonth.dayOfWeek.value % 7).toLong())
-        while (!date.isAfter(lastDayOfMonth)) {
-            dayList += date.dayOfMonth
-            date = date.plusDays(1)
-        }
-        while (dayList.size % 7 != 0) {
-            dayList += date.dayOfMonth
-            date = date.plusDays(1)
-        }
-
-        return dayList
-    }
-
-    private fun initCategoryList() {
-        val db = KakeiboDatabase.getInstance(this)
-        val categories: List<Category> = db.categoryDao().getAll()
-
-        for (category in categories) {
-            if (category.isSpending) {
-                spendingCategoryIds.add(category.id)
-            } else {
-                incomeCategoryIds.add(category.id)
-            }
-        }
-    }
-
-    // TODO DBとの通信回数を減らしたい。一度の通信でクラス変数などにデータを格納しておく?
-    private fun getSpentMoneyText(date: LocalDate): SpannedString {
-        val dateString: String = date.toString()
-
-        val db = KakeiboDatabase.getInstance(this)
-
-        // カテゴリ
-        var spendingTotal:Int = 0
-        var incomeTotal:Int = 0
-
-        for (spending in db.spendingDao().getByDate(dateString)) {
-            if (spending.categoryId in spendingCategoryIds) {
-                spendingTotal += spending.money
-            } else {
-                incomeTotal += spending.money
-            }
-        }
-
-        return buildSpannedString {
-            color(Color.RED) {
-                append("${"%,d".format(spendingTotal)}円\n")
-            }
-            color(Color.BLUE) {
-                append("${"%,d".format(incomeTotal)}円\n")
-            }
-            val savings = incomeTotal - spendingTotal
-            if (savings > 0) {
-                append("+${"%,d".format(savings)}円")
-            } else {
-                append("${"%,d".format(savings)}円")
-            }
-        }
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/kakeibo/fragment/MainFragment.kt
@@ -0,0 +1,223 @@
+package com.example.kakeibo.fragment
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.SpannedString
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
+import androidx.fragment.app.FragmentTransaction
+import com.example.kakeibo.R
+import com.example.kakeibo.database.KakeiboDatabase
+import com.example.kakeibo.entity.Category
+import com.kal.rackmonthpicker.RackMonthPicker
+import com.kal.rackmonthpicker.listener.DateMonthDialogListener
+import com.kal.rackmonthpicker.listener.OnCancelMonthDialogListener
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    private var year: Int = 0
+    private var month: Int = 0
+    private val spendingCategoryIds = mutableListOf<Int>()
+    private val incomeCategoryIds = mutableListOf<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            year = it.getInt("year")
+            month = it.getInt("month")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val addButton = view.findViewById<Button>(R.id.add_button)
+
+        addButton.setOnClickListener {
+            val fragment = AddDataFragment.newInstance("あ", "い")
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.addToBackStack(null)
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+
+        return view
+    }
+    
+    override fun onResume() {
+        super.onResume()
+
+        val view: View = requireView()
+
+        val today: LocalDate = LocalDate.now()
+
+        val date = if (year != 0 && month != 0) {
+            LocalDate.of(year, month, 1)
+        } else {
+            today
+        }
+
+        // 年と月の表示
+        setMonthView(view, date)
+
+        // 表の生成 (日と金額の表示)
+        initCategoryList()
+        val dayList: List<Int> = getDayList(date)
+
+        // 当月の日かどうかのフラグ (先月末から来月頭の日をループで処理するにあたり必要)
+        var thisMonthFlag: Boolean = false
+        for (i in 0 until dayList.size / 7) {
+            for (j in 0..6) {
+                val dayOfMonth = dayList.get(i * 7 + j)
+                if (dayOfMonth == 1) {
+                    thisMonthFlag = !thisMonthFlag
+                }
+
+                val textViewId = resources.getIdentifier("date${i}_${j}", "id", requireContext().packageName)
+                val dateView: TextView = view.findViewById(textViewId)
+                dateView.text = dayList.get(i * 7 + j).toString()
+
+                if (thisMonthFlag) {
+                    val buttonId = resources.getIdentifier("button${i}_${j}", "id", requireContext().packageName)
+                    val button: Button = view.findViewById(buttonId)
+                    val date: LocalDate = date.withDayOfMonth(dayOfMonth)
+                    button.text = getSpentMoneyText(date)
+                    // TODO 遷移
+                    /*button.setOnClickListener {
+                        val intent = Intent(application, DetailActivity::class.java)
+                        intent.putExtra("date", date.format(DateTimeFormatter.ISO_DATE))
+                        startActivity(intent)
+                    }*/
+                } else {
+                    dateView.setTextColor(Color.parseColor("lightgray"))
+                }
+            }
+        }
+    }
+
+    private fun setMonthView(view: View, date: LocalDate) {
+        val monthView: TextView = view.findViewById(R.id.month);
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("YYYY年MM月")
+        monthView.text = date.format(formatter)
+
+        // TODO クリック時の処理
+        monthView.setOnClickListener() {
+            showMonthPickerDialog(date)
+        }
+    }
+
+    private fun showMonthPickerDialog(date: LocalDate) {
+        RackMonthPicker(requireContext())
+            // JAPANESEかも
+            .setLocale(Locale.ENGLISH)
+            // TODO Yearは機能するが、Monthは機能していない (当月が必ず選択される)
+            .setSelectedMonth(date.monthValue)
+            .setSelectedYear(date.year)
+            .setPositiveButton(DateMonthDialogListener
+            { month, startDate, endDate, year, monthLabel ->
+                val fragment = newInstance(year, month)
+                val transaction = parentFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_container, fragment)
+                transaction.commit()
+            })
+            .setNegativeButton(OnCancelMonthDialogListener
+            { dialog ->
+                dialog.dismiss()
+            })
+            .show()
+    }
+
+    // 前後の月を含む、6週分の日のリストを返す (日曜始まり)
+    private fun getDayList(dayOfThisMonth: LocalDate): List<Int> {
+        val firstDayOfMonth: LocalDate = LocalDate.of(dayOfThisMonth.year, dayOfThisMonth.month, 1)
+        val lastDayOfMonth: LocalDate = firstDayOfMonth.plusMonths(1).minusDays(1)
+
+        var dayList: List<Int> = arrayListOf()
+
+        var date: LocalDate = firstDayOfMonth.minusDays((firstDayOfMonth.dayOfWeek.value % 7).toLong())
+        while (!date.isAfter(lastDayOfMonth)) {
+            dayList += date.dayOfMonth
+            date = date.plusDays(1)
+        }
+        while (dayList.size % 7 != 0) {
+            dayList += date.dayOfMonth
+            date = date.plusDays(1)
+        }
+
+        return dayList
+    }
+
+    private fun initCategoryList() {
+        val db = KakeiboDatabase.getInstance(requireContext())
+        val categories: List<Category> = db.categoryDao().getAll()
+
+        for (category in categories) {
+            if (category.isSpending) {
+                spendingCategoryIds.add(category.id)
+            } else {
+                incomeCategoryIds.add(category.id)
+            }
+        }
+    }
+
+    // TODO DBとの通信回数を減らしたい。一度の通信でクラス変数などにデータを格納しておく?
+    private fun getSpentMoneyText(date: LocalDate): SpannedString {
+        val dateString: String = date.toString()
+
+        val db = KakeiboDatabase.getInstance(requireContext())
+
+        // カテゴリ
+        var spendingTotal:Int = 0
+        var incomeTotal:Int = 0
+
+        for (spending in db.spendingDao().getByDate(dateString)) {
+            if (spending.categoryId in spendingCategoryIds) {
+                spendingTotal += spending.money
+            } else {
+                incomeTotal += spending.money
+            }
+        }
+
+        return buildSpannedString {
+            color(Color.RED) {
+                append("${"%,d".format(spendingTotal)}円\n")
+            }
+            color(Color.BLUE) {
+                append("${"%,d".format(incomeTotal)}円\n")
+            }
+            val savings = incomeTotal - spendingTotal
+            if (savings > 0) {
+                append("+${"%,d".format(savings)}円")
+            } else {
+                append("${"%,d".format(savings)}円")
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(year: Int, month: Int) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putInt("year", year)
+                    putInt("month", month)
+                }
+            }
+    }
+}
