003e2319e609ebd91b0415bbe0294718fcb4c710
PoloLavapies <iberia9lavapies@gmail.com>
2023-09-30 14:31:42 +0900

    NavigationDrawer 遷移の実装

--- app/src/main/java/com/example/kakeibo/activity/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo/activity/MainActivity.kt
@@ -2,34 +2,52 @@ package com.example.kakeibo.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import com.example.kakeibo.R
+import com.google.android.material.navigation.NavigationView
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
         val toolBar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolBar)
 
         val navController = findNavController(R.id.nav_host_fragment)
         navController.setGraph(R.navigation.navigation_graph)
 
-        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph)
-            .setOpenableLayout(findViewById<View>(R.id.drawer_layout) as DrawerLayout)
-            .build()
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolBar, 0, 0
+        )
+        drawer.addDrawerListener(toggle);
+        toggle.syncState()
 
-        NavigationUI.setupWithNavController(toolBar, navController, appBarConfiguration)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_home -> findNavController(R.id.nav_host_fragment).navigate(R.id.MainFragment)
+            // TODO Fragmentとviewを作成
+            R.id.menu_setting -> println("カテゴリー設定画面へ遷移します")
+        }
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
